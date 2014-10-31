7d4d45aba3fc73f9e832fba889e87331a49641e7
hujiangtao <hujiangtao@baidu.com>
2014-10-31 14:23:44 -0700

    dec to excel conversion.

---
+++ interview/convert.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+var decToExcel = function (n) {
+	var s = "";
+	var d = 0;
+	while (n > 0) {
+		d = n % 26
+		if (n == 26) {
+			n = 0
+		} else {
+			n = Math.floor(n / 26)
+		}
+		d = (d == 0 ? 26 : d)
+		s = String.fromCharCode(d - 1 + "A".charCodeAt()) + s;
+	}
+	return s;
+}
+
+var excelToDec = function (s) {
+	var n = 0;
+	s = s.toUpperCase();
+	var i = 0;
+	while (i < s.length) {
+		n = n * 26 + (1 + s.charCodeAt(i) - "A".charCodeAt());
+		i++;
+	}
+	return n
+}
+
+for (var i = 1; i < 100; i++) {
+	console.log("dec <=> Execl:", i, "=>", decToExcel(i), "=>", excelToDec(decToExcel(i)));
+}
