f26bf646e9a57086a5566163a18a57c2e9787fae
boris.hu <boris.hu@gmail.com>
2015-05-16 11:57:05 -0700

    add / remove player function.

--- meteor/leaderboard/leaderboard.js
+++ meteor/leaderboard/leaderboard.js
@@ -11,11 +11,11 @@ if (Meteor.isClient) {
       return PlayersList.find({}, {sort: {score: -1, name: 1} });
     },
     'selectedClass': function(){
-        var playerId = this._id;
-        var selectedPlayer = Session.get('selectedPlayer');
-        if(playerId == selectedPlayer){
-            return "selected";
-        }
+      var playerId = this._id;
+      var selectedPlayer = Session.get('selectedPlayer');
+      if(playerId == selectedPlayer){
+        return "selected";
+      }
     }, 
     'showSelectedPlayer': function(){
       var selectedPlayer = Session.get('selectedPlayer');
@@ -30,13 +30,30 @@ if (Meteor.isClient) {
     }, 
     'click .increment': function(){
      var selectedPlayer = Session.get('selectedPlayer');
-      PlayersList.update(selectedPlayer, {$inc: {score: 5} });
-    },
-    'click .decrement': function(){
-      var selectedPlayer = Session.get('selectedPlayer');
-      PlayersList.update(selectedPlayer, {$inc: {score: -5} });
+     PlayersList.update(selectedPlayer, {$inc: {score: 5} });
+   },
+   'click .decrement': function(){
+    var selectedPlayer = Session.get('selectedPlayer');
+    PlayersList.update(selectedPlayer, {$inc: {score: -5} });
+  },
+  'click .remove': function(){
+    var selectedPlayer = Session.get('selectedPlayer');
+    PlayersList.remove(selectedPlayer);
+  }
+});
+
+
+  Template.addPlayerForm.events({
+    'submit form': function(event){
+      event.preventDefault();
+      var playerNameVar = event.target.playerName.value;
+      PlayersList.insert({
+        name: playerNameVar,
+        score: 0
+      });
     }
-  });
+ });
+
 }
 
 if (Meteor.isServer) {
